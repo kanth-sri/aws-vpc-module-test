cdf6948e2cbd794a9005463c32bdabd50e2424ff
kanth-sri <srikanthadepu8008@gmail.com>
2024-09-26 01:26:30 +0530

    aws vpc tf custom module test

--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
 # output "AZ_list" {
 #   value = module.vpc.az_info
 # }
+# output "vpc_info" {
+#   value = module.vpc.default_vpc
+# }
--- vars.tf
+++ vars.tf
@@ -1,3 +1,6 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
 variable "project" {
   default = "expense"
 }
